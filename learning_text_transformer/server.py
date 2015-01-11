3778b5a00a85112f5b7f4d7a9c60aa5bd8015ec0
Ian Ozsvald <ian@ianozsvald.com>
2015-01-11 20:52:57 +0000

    start of logging

--- learning_text_transformer/server.py
+++ learning_text_transformer/server.py
@@ -10,6 +10,17 @@ app = Flask(__name__)
 api = restful.Api(app)
 
 
+
+def log_learn_entry(examples_to_learn_from):
+    pass
+
+def log_learn_exit(result, best_score):
+    pass
+
+def log_transform_entry(inputs):
+    pass
+
+
 class HelloWorld(restful.Resource):
     def get(self):
         return {'learning': 'server'}
@@ -33,12 +44,15 @@ class Learn(restful.Resource):
         print("reqs:", reqs)
         self.check_inputs_or_abort(reqs)
         examples_to_learn_from = list(zip(reqs['inputs'], reqs['outputs']))
+        log_learn_entry(examples_to_learn_from)
+        best_score = None
         if examples_to_learn_from:
             transform_searcher = learner.get_transform_searcher()
             chosen_transformations, best_score = transform_searcher.search_and_find_best_sequence(examples_to_learn_from)
         else:
             chosen_transformations = []
         result = make_learn_result(chosen_transformations)
+        log_learn_exit(result, best_score)
         return jsonify(result)
 api.add_resource(Learn, '/learn')
 
@@ -48,6 +62,7 @@ class Transform(restful.Resource):
         reqs = request.get_json()
         serialisation = transforms.Serialisation()
         inputs = reqs['inputs']
+        #log_transform_entry(inputs)
         outputs = []
         for s in inputs:
             ts_raw = reqs['transforms']
